e08e83e01a251a73fc95e89f72e01d7382c9cf1c
Derimar Gray <derimargray@gmail.com>
2021-12-14 17:33:38 -0500

    Values and Variables

--- 01-Fundamentals-Part-1/project/assignments.js
+++ 01-Fundamentals-Part-1/project/assignments.js
@@ -1,8 +1,17 @@
 // JavaScript Fundamentals - Part 1 
 
 
-// LECTURE: Values and Variables
+// ------- LECTURE: Values and Variables ----------
 
 /* 1. Declare variables called 'country', 'continent', and 'population' and assing their values according to your own country (population in millions)
 */
 
+let country = 'United States';
+let continent = 'North America';
+let population = 333849235;
+
+/* 2. Log their values to the console */
+console.log("country: " + country);
+console.log("continent: " + continent);
+console.log("population: " + population);
+console.log("The country of " + country + " is part of the continent of " + continent + " and has a population of " + population);
