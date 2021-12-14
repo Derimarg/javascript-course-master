7455a1f2283d8478629ad9c8c9feee48596abc55
Derimar Gray <derimargray@gmail.com>
2021-12-14 17:16:12 -0500

    initial setup

---
+++ 01-Fundamentals-Part-1/project/assignments.js
@@ -0,0 +1,8 @@
+// JavaScript Fundamentals - Part 1 
+
+
+// LECTURE: Values and Variables
+
+/* 1. Declare variables called 'country', 'continent', and 'population' and assing their values according to your own country (population in millions)
+*/
+
---
+++ 01-Fundamentals-Part-1/project/script.js
@@ -0,0 +1,6 @@
+let js = "amazing";
+if (js === "amazing") alert("JavaScript is FUN");
+
+const sum = 40 + 8 + 23 - 10;
+
+console.log(sum);
