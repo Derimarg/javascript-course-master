a562776a6284311a607828e76794bcdfa163d050
Derimar Gray <derimargray@gmail.com>
2021-12-27 17:55:57 -0600

    LECTURE: Equality Operators: == vs. ===

--- 01-Fundamentals-Part-1/module/lectures/lectures.js
+++ 01-Fundamentals-Part-1/module/lectures/lectures.js
@@ -164,7 +164,6 @@ console.log("23" / "2");
 let n = "1" + 1; // '11'
 n = n - 1; // '11' - 1 = 10
 console.log(n);
-*/
 
 // ------- LECTURE:  Truthy and falsy values -------
 // 5 falsy values: 0, '', undefined, null, NaN
@@ -189,3 +188,27 @@ if (height) {
   console.log('Height is UNDEFINED');
 }
 
+*/
+
+// ------- LECTURE:  Equality Operators: == vs ===  -------
+
+const age = "18";
+if (age === 18) console.log("You just became  an adult :D (strict");
+
+if (age == 18) console.log("You just became an adult ;D (loose)");
+
+const favorite = Number(prompt("What's your favorite number?"));
+console.log(favorite);
+console.log(typeof favorite);
+
+if (favorite === 23) {
+  console.log("Cool! 23 is an amazing number!");
+} else if (favorite === 7) {
+  console.log("7 is also a cool number");
+} else if (favorite === 9) {
+  console.log("9 is also a cool number");
+} else {
+  console.log("Number is not 23 or 7 or 9");
+}
+
+if (favorite !== 23) console.log('Why not 23?');
