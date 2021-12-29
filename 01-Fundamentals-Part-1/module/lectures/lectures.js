f6fa76592666a9eb477b1254fd426591f97a8e95
Derimar Gray <derimargray@gmail.com>
2021-12-28 18:21:29 -0600

    Logical Operators

--- 01-Fundamentals-Part-1/module/lectures/lectures.js
+++ 01-Fundamentals-Part-1/module/lectures/lectures.js
@@ -188,7 +188,6 @@ if (height) {
   console.log('Height is UNDEFINED');
 }
 
-*/
 
 // ------- LECTURE:  Equality Operators: == vs ===  -------
 
@@ -212,3 +211,28 @@ if (favorite === 23) {
 }
 
 if (favorite !== 23) console.log('Why not 23?');
+*/
+
+// ------- LECTURE:  Logical Operators -------
+
+const hasDriversLicense = true; // A
+const hasGoodVision = false; // B
+
+console.log(hasDriversLicense && hasGoodVision);
+console.log(hasDriversLicense || hasGoodVision);
+console.log(!hasDriversLicense);
+
+if (hasDriversLicense && hasGoodVision) {
+  console.log('Sarah is able to drive!');
+} else {
+  console.log('Something else should drive...');
+}
+
+const isTired = true; // C
+console.log(hasDriversLicense || hasGoodVision || isTired);
+
+if (hasDriversLicense && hasGoodVision && !isTired) {
+  console.log('Sarah is able to drive!');
+} else {
+  console.log('Something else should drive...');
+}
